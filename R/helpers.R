afe1b158786fa94f1fcddca6b5c2a9f14b645460
tori-cowger <vcowger@hsph.harvard.edu>
2025-12-02 10:48:13 -0800

    Updating function descriptions

--- R/helpers.R
+++ R/helpers.R
@@ -4,6 +4,12 @@
 `%ni%` <- purrr::negate(`%in%`) # For filtering list items not in
 
 # Function that takes a PC copied file location (with forward slashes) and converts to R readable path
+#' Format copied PC file location for universal use
+#'
+#' @returns After copying to clipboard, returns string formatted with standard file path formatting
+#' @export
+#'
+#' @examples
 copied_path <- function(){
   out <- utils::readClipboard()
   out <- gsub("^\"|\"$", "", out)
--- R/setup.R
+++ R/setup.R
@@ -1,6 +1,12 @@
 # Setup
 
 # For Negate %ni% function uses purr but not recognized so import any function
+#' Ignore Unused Imports
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 ignore_unused_imports <- function() {
   purrr::negate()
 }
