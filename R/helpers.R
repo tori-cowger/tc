8077ee23da4523ad58bada28f31dcf3c5b2bdafd
tori-cowger <vcowger@hsph.harvard.edu>
2025-12-02 10:32:47 -0800

    adding copied path function and documentation

--- R/helpers.R
+++ R/helpers.R
@@ -1,2 +1,15 @@
+
 # Negate Function
+# Opposite of %in%
 `%ni%` <- purrr::negate(`%in%`) # For filtering list items not in
+
+# Function that takes a PC copied file location (with forward slashes) and converts to R readable path
+copied_path <- function(){
+  out <- readClipboard()
+  out <- gsub("^\"|\"$", "", out)
+  out <- gsub("\\\\", "/", out)
+  # clipr::write_clip(out) #
+  writeClipboard(out)
+  message("The formatted path has been copied to your clipboard.")
+  return(out)
+}
